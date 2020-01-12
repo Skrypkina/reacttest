652a16dcadd71c5d86eb7043ccb8e5ed2372b61f
Larysa <Larysa76@gmail.com>
2020-01-12 13:09:58 +0200

    Updates

---
+++ precache-manifest.0e1e956c286bfde95ebe876c9b2e3301.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01da462f96559e82b227119ad2bdd0cb",
+    "url": "/reacttest/index.html"
+  },
+  {
+    "revision": "b5a9ae97aea0dcd50e19",
+    "url": "/reacttest/static/js/2.b808a298.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/reacttest/static/js/2.b808a298.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f85ba81bf768292376e9",
+    "url": "/reacttest/static/js/main.64632444.chunk.js"
+  },
+  {
+    "revision": "ac6404c32ff83b455ebd",
+    "url": "/reacttest/static/js/runtime-main.314342db.js"
+  }
+]);
